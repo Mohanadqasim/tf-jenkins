751b11b83cef29ecb6bd5a9be95a5bc667422adc
mohanadqasim <mohanad.qasim@hotmail.com>
2026-03-05 16:33:19 +0300

    test pipeline

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return jsonify(message="test pipeline")
+    return jsonify(message="test pipeline 2")
 
 @app.route("/health")
 def health():
